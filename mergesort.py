29cb3b6a102b8f5449f552441b238ca7a1920005
adiii3692 <adinair0206@gmail.com>
2024-09-25 09:50:47 -0400

    Learnt merge sort

--- mergesort.py
+++ mergesort.py
@@ -10,7 +10,31 @@ def mergesort(arr: list[int])->list[int]:
         mergesort(rightArr)
 
         #Now you merge the arrays
+        i = 0 #left array index
+        j = 0 #Right array index
+        k = 0 #Index for merged array
+
+        #Compare the left array and the right array and move the smaller one to the merged array
+        while(i<len(leftArr) and j<len(rightArr)):
+            if (leftArr[i]<rightArr[j]):
+                arr[k] = leftArr[i]
+                i +=1
+            else:
+                arr[k] = rightArr[j]
+                j += 1
+            k += 1
         
+        #Once you've transferred all values from one of the arrays, add the remaining ones to the merged array
+        while (i<len(leftArr)):
+            arr[k] = leftArr[i]
+            i += 1
+            k += 1
+        while(j<len(rightArr)):
+            arr[k] =  rightArr[j]
+            j += 1
+            k += 1
+
+             
 
     return arr
 
