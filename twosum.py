e5688d90cc476d9471f5388e7b1264e829fbb983
adiii3692 <adinair0206@gmail.com>
2024-09-26 09:44:43 -0400

    Redid two sum

--- twosum.py
+++ twosum.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        #Create a hashmap and store the indices of the various integers
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         h = {}
-        for index,i in enumerate(nums):
-            h[i] = index
+        for i in range(len(nums)):
+            h[nums[i]] = i
         
-
         for i in range(len(nums)):
             needed = target - nums[i]
             if needed in h and h[needed]!=i:
                 return [i,h[needed]]
+        
+        return [-1,-1]
