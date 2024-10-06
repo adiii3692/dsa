bd2e549c694a56d460e0e97b3be4007760929335
adiii3692 <adinair0206@gmail.com>
2024-10-06 14:40:16 -0500

    Mastered merge sort

--- practice.py
+++ practice.py
@@ -1,3 +1,28 @@
 def mergesort(nums: list[int])->list[int]:
     if len(nums)>1:
-        leftArr = 
+        leftArr = nums[:len(nums)//2]
+        rightArr = nums[len(nums)//2:]
+        
+        i=j=k=0
+        
+        while i<len(leftArr) and j<len(rightArr):
+            if leftArr[i] < rightArr[j]:
+                nums[k] = leftArr[i]
+                i += 1
+            else:
+                nums[k] = rightArr[j]
+                j += 1
+            k += 1
+        
+        while i<len(leftArr):
+            nums[k] = leftArr[i]
+            k += 1
+            i += 1
+        
+        while j<len(rightArr):
+            nums[k] = rightArr[j]
+            k += 1
+            j += 1
+        
+        
+    return nums
