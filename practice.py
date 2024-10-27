8f5be318f11c5ca399e019fe3998470fc8979d6b
adiii3692 <adinair0206@gmail.com>
2024-10-26 23:25:05 -0500

    Back at it

--- practice.py
+++ practice.py
@@ -1,20 +1,22 @@
-def mergesort(nums: list[int])->list[int]:
+def mergesort(nums:list[int])->list[int]:
     if len(nums)>1:
         leftArr = nums[:len(nums)//2]
         rightArr = nums[len(nums)//2:]
         
+        #Recursion
         mergesort(leftArr)
         mergesort(rightArr)
         
-        i=j=k=0
+        #Merge
+        i = j = k = 0
         
-        while i<len(leftArr) and j<len(rightArr):
-            if leftArr[i] < rightArr[j]:
+        while(i<len(leftArr) and j<len(rightArr)):
+            if leftArr[i]<rightArr[j]:
                 nums[k] = leftArr[i]
                 i += 1
             else:
                 nums[k] = rightArr[j]
-                j += 1
+                j +=1 
             k += 1
         
         while i<len(leftArr):
@@ -26,27 +28,34 @@ def mergesort(nums: list[int])->list[int]:
             nums[k] = rightArr[j]
             k += 1
             j += 1
-        
-        
-    return nums
+    
+nums = [14, 61, 83, 44, 6,
+47, 4, 61, 52, 55,
+24, 63, 69, 44, 43,
+35, 78, 50, 74, 52,
+9, 76, 93, 57, 16,
+45, 21, 73, 7, 72,
+63, 13, 50, 14, 26,
+56, 71, 66, 87, 21,
+99, 69, 50, 3, 65,
+27, 60, 91, 30, 77]
 
-def binarySearch(nums:list[int], target: int)->int:
+mergesort(nums)    
+
+def binarySearch(nums:list[int],target:int)->int:
     l,r = 0,len(nums)-1
     
-    while l<=r:
+    while(l<=r):
         m = (l+r)//2
-        
         if nums[m]<target:
             l = m+1
-        elif nums[m] >target:
+        elif nums[m]>target:
             r = m-1
         else:
             return m
-    
     return -1
 
-arr = [330, 336, 367, 974, 449, 294, 194, 769, 869, 261, 919, 197, 556, 843, 503, 943, 601, 541, 265, 992, 632, 177, 496, -51, 619, 149, 71, 349, 740, -55, 702, 702, 673, 584, 771, 397, 581, 417, -46, 740, 914, 791, -80, -64, -5, 902, 671, 623, 249, 483, 233, 27, 379, 388, 741, 102, 388, 554, 20, 82, 815, 188, 639, -42, 833, 550, 938, 809, 961, 96, 7, 810, 505, 151, 634, 734, 559, 141, 639, 561, -40, 195, 444, 371, -93, 342, 260, 600, 456, 765, 358, 415, 682, 512, 135, 153, 749, 258, 465, -74]
+print(nums)
+print("Target index: ",nums[binarySearch(nums,77)])
+        
 
-mergesort(arr)
-
-print(binarySearch(arr,-74))
