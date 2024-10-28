92285fe049efcc49b87d0c36b7f253a3e5867b8f
adiii3692 <adinair0206@gmail.com>
2024-10-27 21:12:24 -0500

    Learnt two pointers for removing duplicates

--- duplicate.py
+++ duplicate.py
@@ -1,15 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        index = 0
-        h = []
-        for i in range(len(nums)):
-            if nums[i] not in h:
-                h.append(nums[i])
-                nums[index] = nums[i]
-                index += 1
-        print("nums: ",nums)
-        return index
-
+        l = 1
+        for i in range(1,len(nums)):
+            if nums[i]!= nums[i-1]:
+                nums[l] = nums[i]
+                l += 1
+        
 s = Solution()
 nums = [0,0,1,1,1,2,2,3,3,4]
 s.removeDuplicates(nums)
+print(nums)
