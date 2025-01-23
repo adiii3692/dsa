cc34e5894c6198442bf07129ba035c83589e0616
adiii3692 <adinair0206@gmail.com>
2025-01-23 13:16:33 -0500

    Got an optimal solution

--- gcdarray.cpp
+++ gcdarray.cpp
@@ -23,53 +23,13 @@ public:
 
         return gcd(a,(b-a));
     }
-
-    void mergeSort(vector<int>& nums){
-        if(nums.size()>1){
-            vector<int> left = vector<int>(nums.begin(),nums.end()-(nums.size()/2));
-            vector<int> right;
-            if(nums.size()%2==0){
-                right = vector<int>(nums.begin()+(nums.size()/2),nums.end());
-            }
-            else{
-                right = vector<int>(nums.begin()+(nums.size()/2)+1,nums.end());
-            }
-
-            //Recursion
-            mergeSort(left);
-            mergeSort(right);
-
-            //Merge back
-            int i = 0, j = 0, k = 0;
-
-            while(i<left.size() && j<right.size()){
-                if(left[i]<right[j]){
-                    nums[k] = left[i];
-                    i += 1;
-                    k += 1;
-                }
-                else{
-                    nums[k] = right[j];
-                    j += 1;
-                    k += 1;
-                }
-            }
-
-            while(i<left.size()){
-                nums[k] = left[i];
-                k += 1;
-                i += 1;
-            }
-
-            while(j<right.size()){
-                nums[k] = right[j];
-                k += 1;
-                j += 1;
-            }
-        }
-    }
+  
     int findGCD(vector<int>& nums) {
-        mergeSort(nums);
-        return gcd(nums[0],nums[nums.size()-1]);
+        int low = 1001, high = 0;
+        for(int e: nums){
+            if(e>high) high = e;
+            if(e<low) low = e;
+        }
+        return gcd(low,high);
     }
 };
