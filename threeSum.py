a0340289c2709f018de2532a58f6f62bb85c34e6
adiii3692 <adinair0206@gmail.com>
2025-02-25 09:47:36 -0500

    Completed 3Sum on Leetcode using two pointers

--- threeSum.py
+++ threeSum.py
@@ -1,9 +1,27 @@
-stru = "leetscode"
-dictionary = ["leet","code","leetcode"]
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
 
-solution = []
-for i in dictionary:
-    start = stru.find(i)
-    end = start + len(i) - 1 
+        result = list()
 
-print(solution)
+        for i,num in enumerate(nums):
+            if num>0:
+                break
+            if i>0 and num == nums[i-1]:
+                continue
+            
+            l,r = i + 1, len(nums) - 1
+
+            while l<r:
+                sum = num + nums[l] + nums[r]
+                if sum > 0:
+                    r -= 1
+                elif sum < 0:
+                    l += 1
+                else:
+                    result.append([num, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l<r:
+                        l += 1
+        return result
